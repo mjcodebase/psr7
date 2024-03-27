eccfe9705dfa882ec80c1cb3ab4cf92787d1b320
Michael Johnson <159731452+mjohnsoncode@users.noreply.github.com>
2024-03-28 00:50:40 +0300

    Update DroppingStream.php

--- src/DroppingStream.php
+++ src/DroppingStream.php
@@ -22,7 +22,7 @@ class DroppingStream extends StreamDecoratorTrait implements StreamInterface
         $this->maxLength = $maxLength;
     }
 
-    public function write($string)
+    public function write($string): int
     {
         $diff = $this->maxLength - $this->stream->getSize();
 
